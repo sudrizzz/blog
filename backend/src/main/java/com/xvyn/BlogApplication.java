264d6d35dff78d704d78c0179fd4be616d4fbfd4
xvyn <sudrizzz@gmail.com>
2020-02-21 21:08:32 +0800

    update

--- backend/src/main/java/com/xvyn/BlogApplication.java
+++ backend/src/main/java/com/xvyn/BlogApplication.java
@@ -3,6 +3,8 @@ package com.xvyn;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -10,11 +12,15 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @MapperScan("com.xvyn.mapper")
 @EnableCaching
 @EnableTransactionManagement
-public class BlogApplication {
+public class BlogApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
 }
 
